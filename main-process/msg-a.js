10b32926b8a1017d434cc9d28185829f4d205e5f
ccfine <469781947@qq.com>
2018-03-22 18:42:06 +0800

    electron-project

---
+++ main-process/msg-a.js
@@ -0,0 +1,10 @@
+const { ipcMain } = require("electron");
+
+ipcMain.on("msg-a", (e) => {
+  e.returnValue = "hello hello";
+});
+
+ipcMain.on("msg-a-1", (e, arg) => {
+  console.log(arg.name);
+  e.sender.send("msg-a-1-back", { name: "chen"});
+});
