e499a33a36c63df435b7424591427b21ec2c8131
ccfine <469781947@qq.com>
2018-03-26 16:46:10 +0800

    electron-project

--- render-process/clipboard.js
+++ render-process/clipboard.js
@@ -4,7 +4,7 @@ clipboard.writeText("1");
 clipboard.writeText("2");
 console.log(clipboard.readText());
 
-let img = nativeImage.createFromPath("main-process/1.png");
+let img = nativeImage.createFromPath("../main-process/1.png");
 clipboard.writeImage(img);
 const imgDataURL = clipboard.readImage().toDataURL();
 
